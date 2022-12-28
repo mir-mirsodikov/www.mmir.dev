8c2a1ef22e031d11daecebfd28a62a742fd3e718
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-12-28 10:38:25 -0600

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from './page.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <main>
-      <div className="p-8 m-auto w-3/4 mt-24 min-h-screen">
+      <div className="p-8 m-auto w-3/4 mt-48 -mb-48 min-h-screen">
         <h1 className="text-l text-slate-300 mb-7">Hello, my name is</h1>
         <p className="mb-2 text-7xl text-indigo-500 font-bold">
           Mirmukhammad Mirsodikov.
@@ -22,20 +16,22 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="p-8 m-auto w-3/4 min-h-screen" id='about'>
-        <h1 className="text-left text-slate-300 text-3xl font-bold mt-16">About</h1>
+      <div className="p-8 m-auto w-3/4" id='about'>
+        <h1 className="text-left text-slate-300 text-3xl font-bold">About</h1>
         <hr className="border-slate-600 mt-2" />
         <p className='text-slate-400 w-1/2 mt-4'>
           My name is Mirmukhammad Mirsodikov and I am a software engineer.
         </p>
       </div>
 
-      <div className="p-8 m-auto w-3/4">
-        <h1 className="text-left text-slate-300 text-3xl">Work Experience</h1>
+      <div className="p-8 m-auto w-3/4" id='work'>
+        <h1 className="text-left text-slate-300 text-3xl mt-16">Work Experience</h1>
+        <hr className="border-slate-600 mt-2" />
       </div>
 
-      <div className="p-8 m-auto w-3/4">
-        <h1 className="text-left text-slate-300 text-3xl">Projects</h1>
+      <div className="p-8 m-auto w-3/4" id='projects'>
+        <h1 className="text-left text-slate-300 text-3xl mt-16">Projects</h1>
+        <hr className="border-slate-600 mt-2" />
       </div>
     </main>
   );
