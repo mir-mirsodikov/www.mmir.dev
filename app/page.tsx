88656ef57f22f3c28f177ebdb8e932be6b96b451
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-12-28 16:19:46 -0600

    Added work experience section outline

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import portrait from '../public/pic.jpeg';
+
 export default function Home() {
   return (
     <main>
@@ -16,20 +19,56 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="p-8 m-auto w-3/4" id='about'>
-        <h1 className="text-left text-slate-300 text-3xl font-bold">About</h1>
+      <div className="p-8 m-auto w-3/4" id="about">
+        <h1 className="text-left text-slate-300 text-3xl font-bold mt-16">
+          About
+        </h1>
         <hr className="border-slate-600 mt-2" />
-        <p className='text-slate-400 w-1/2 mt-4'>
-          My name is Mirmukhammad Mirsodikov and I am a software engineer.
-        </p>
+        <div className="flex flex-row justify-between">
+          <div>
+            <p className="text-slate-400 mt-4">
+              My name is Mirmukhammad Mirsodikov and I am a software engineer.
+            </p>
+          </div>
+          <div className="items-start">
+            <Image
+              src={portrait}
+              alt="Self portrait"
+              className="mt-4 mr-8 w-96"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="p-8 m-auto w-3/4" id='work'>
-        <h1 className="text-left text-slate-300 text-3xl mt-16">Work Experience</h1>
+      <div className="md:p-8 m-auto md:w-3/4 w-full p-4" id="work">
+        <h1 className="text-left text-slate-300 text-3xl mt-16 font-bold">
+          Work Experience
+        </h1>
         <hr className="border-slate-600 mt-2" />
+        <div className='mt-8 grid md:grid-cols-2 gap-4'>
+          <div className='bg-slate-800 p-4 rounded-md'>
+            <h2 className="text-slate-300 text-xl font-semibold">StaffWise - Software Engineer</h2>
+            <p className='text-slate-400 text-sm'>July 2021 - Present</p>
+            <ul className='list-disc text-slate-300 ml-4 mt-4'>
+              <li>Alpha bravo charlie</li>
+              <li>Alpha bravo charlie</li>
+              <li>Alpha bravo charlie</li>
+            </ul>
+          </div>
+          <div className='bg-slate-800 p-4 rounded-md'>
+            <h2 className="text-slate-300 text-xl font-semibold">Textron Aviation - Software Engineer (Intern)</h2>
+            <p className='text-slate-400 text-sm'>June 2022 - August 2022</p>
+            <ul className='list-disc text-slate-300 ml-4 mt-4'>
+              <li>Alpha bravo charlie</li>
+              <li>Alpha bravo charlie</li>
+              <li>Alpha bravo charlie</li>
+            </ul>
+          </div>
+        </div>
+        <div></div>
       </div>
 
-      <div className="p-8 m-auto w-3/4" id='projects'>
+      <div className="p-8 m-auto w-3/4" id="projects">
         <h1 className="text-left text-slate-300 text-3xl mt-16">Projects</h1>
         <hr className="border-slate-600 mt-2" />
       </div>
