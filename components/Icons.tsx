33162cb1c3d2ec7d49744854a80c416f9d16acb2
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-12-29 00:39:50 -0600

    Added custom favicon. Removed icons that came with next

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -30,12 +30,12 @@ export function LaunchIcon({ link }: IconProps) {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         className="w-6 h-6 stroke-slate-400 hover:stroke-indigo-500"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
         />
       </svg>
@@ -50,7 +50,6 @@ export function RepoLinkIcon({ link }: IconProps) {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        strokeWidth={1.5}
         className="w-6 h-6 stroke-slate-400 hover:stroke-indigo-500"
       >
         <path
