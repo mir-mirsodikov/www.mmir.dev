1e4371a16a992e5f14325291188cde818d251185
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2023-04-12 14:17:56 -0500

    Separated work experience into a JSON and added new experience

--- components/sections/Work.tsx
+++ components/sections/Work.tsx
@@ -1,67 +1,43 @@
+import WorkExperienceJSON from '../../assets/experience.json';
 import { Card } from '../Card';
 import { Section } from '../Section';
 import { SkillCard } from '../SkillCard';
 
-export function WorkExperience() {
-  const staffWiseSkills = [
-    'TypeScript',
-    'React',
-    'Node.js',
-    'Terraform',
-    'Microsoft SQL Server',
-    'Microsoft Azure',
-  ];
+interface Experience {
+  company: string;
+  title: string;
+  employmentDates: string;
+  description: string[];
+  skills: string[];
+}
 
-  const textronSkills = ['Java', 'Java Swing', 'MVVM', 'REST'];
+export function WorkExperience() {
+  const workExperience = WorkExperienceJSON as Experience[];
+
   return (
     <Section id="work" title="Work Experience">
       <div className="mt-8 grid md:grid-cols-2 gap-4">
-        <Card title="StaffWise - Software Engineer">
-          <p className="text-slate-400 text-sm">July 2021 - Present</p>
-          <ul className="list-disc text-slate-300 ml-4 mt-4 space-y-2 text-base mb-2">
-            <li>
-              Designed and built REST APIs to power a healthcare financial web
-              application for budgeting and staffing.
-            </li>
-            <li>
-              Constructed database relations and procedures in Microsoft SQL
-              Server.
-            </li>
-            <li>
-              Architected resources in Microsoft Azure using Terraform to
-              automate resource deployments.
-            </li>
-            <li>
-              Implemented UI using React and its ecosystem to deliver
-              exceptional user experiences.
-            </li>
-          </ul>
+        {workExperience.map((experience) => (
+          <Card
+            key={experience.company}
+            title={`${experience.company} - ${experience.title}`}
+          >
+            <p className="text-slate-400 text-sm">
+              {experience.employmentDates}
+            </p>
+            <ul className="list-disc text-slate-300 ml-4 mt-4 space-y-2 text-base mb-2">
+              {experience.description.map((description, index) => (
+                <li className="text-slate-300 text-base mt-4" key={index}>
+                  {description}
+                </li>
+              ))}
+            </ul>
 
-          {staffWiseSkills.map((skill) => (
-            <SkillCard key={skill} title={skill} />
-          ))}
-        </Card>
-        <Card title="Textron Aviation - Software Engineer (Intern)">
-          <p className="text-slate-400 text-sm">June 2022 - August 2022</p>
-          <ul className="list-disc text-slate-300 ml-4 mt-4 space-y-2 text-base mb-2">
-            <li>
-              Developed applications using Java that automate processes for
-              engineers in their design modelling application.
-            </li>
-            <li>
-              Addressed prevalent problems of previous projects with clear and
-              maintainable solutions by enforcing application architecture and
-              design patterns
-            </li>
-            <li>
-              Lead the team in migrating to Java 11 from Java 8 and adopting
-              REST APIs over SOAP APIs
-            </li>
-          </ul>
-          {textronSkills.map((skill) => (
-            <SkillCard key={skill} title={skill} />
-          ))}
-        </Card>
+            {experience.skills.map((skill) => (
+              <SkillCard key={skill} title={skill} />
+            ))}
+          </Card>
+        ))}
       </div>
     </Section>
   );
