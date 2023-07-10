5755e1f5bf0ce48bde9cfba3ba017379ade7a502
Mirsodikov <mirmukhammad.mirsodikov@kochind.com>
2023-07-10 12:49:26 -0500

    Added extra info to the work json and added animations

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,17 +1,26 @@
+'use client';
+import { motion } from "framer-motion";
+
 interface SectionProps {
   id: string;
   children: React.ReactNode;
   title: string;
+  fullScreen?: boolean;
 }
 
-export function Section({ id, children, title }: SectionProps) {
+export function Section({ id, children, title, fullScreen = false }: SectionProps) {
   return (
-    <div className="lg:pd-8 m-auto md:w-full lg:w-full xl:w-3/4 lg:p-8 w-full xl:p-4 p-4 " id={id}>
+    <motion.div 
+    initial={{ y: 200, opacity: 0 }}
+    whileInView={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.7 }}
+    viewport={{ once: true }}
+    className={`lg:pd-8 m-auto md:w-full lg:w-full xl:w-3/4 lg:p-8 w-full xl:p-4 p-4 ${fullScreen ? 'min-h-screen' : ''}`} id={id}>
       <h1 className="text-left text-slate-200 text-3xl mt-16 font-bold">
         {title}
       </h1>
       <hr className="border-slate-600 mt-2" />
       {children}
-    </div>
+    </motion.div>
   );
 }
