3c7189abba2527d85340cd591debe7989578c513
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2023-11-01 22:49:05 -0500

    Updated head

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { AnalyticsWrapper } from '../components/Analytics';
 import { NavBar } from '../components/navigation/NavBar';
 import { NavigationDrawer } from '../components/navigation/NavigationDrawer';
 import './globals.css';
+import Head from './head';
+
 
 export default function RootLayout({
   children,
@@ -19,7 +21,7 @@ export default function RootLayout({
       lang="en"
       className={`${justLoaded ? `overflow-hidden` : 'overflow-auto'}`}
     >
-      <head />
+      <Head />
       <body className={`bg-slate-900 font-mono`} id="home">
         <div className={`md:flex hidden md:sticky md:top-0 md:z-50`}>
           <NavBar />
